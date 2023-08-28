ba37b58958be2fb9cb9adda7aa2721899fbed10f
Syahrul <syahrul4w@gmail.com>
2023-08-28 22:12:31 +0700

    fix belt

--- src/component/belt.tsx
+++ src/component/belt.tsx
@@ -5,27 +5,12 @@ export const Belt = () => {
         className="flex items-center justify-center gap-4 w-[100vw] h-[7.8rem] flex-nowrap overflow-hidden"
         id="belt-parenet"
       >
-        <div className="belt1 flex w-full">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 50 51"
-            className="fill-none w-40 h-40"
-          >
-            <path
-              d="M25.0854 0.80481C25.0854 25.5 49.7806 25.5 49.7806 25.5C49.7806 25.5 25.0854 25.5 25.0854 50.1951C25.0854 25.5 0.390287 25.5 0.390287 25.5C0.390287 25.5 25.0855 25.5 25.0854 0.80481Z"
-              fill="white"
-            />
-          </svg>
-
-          <p className="text-white font-bebas-neue text-[4rem]">
-            MAKE YOU HEALHTY
-          </p>
-        </div>
-
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="fill-none w-40 h-40"
+          width="50"
+          height="51"
           viewBox="0 0 50 51"
+          fill="none"
         >
           <path
             d="M25.0854 0.80481C25.0854 25.5 49.7806 25.5 49.7806 25.5C49.7806 25.5 25.0854 25.5 25.0854 50.1951C25.0854 25.5 0.390287 25.5 0.390287 25.5C0.390287 25.5 25.0855 25.5 25.0854 0.80481Z"
@@ -33,13 +18,32 @@ export const Belt = () => {
           />
         </svg>
 
-        <p className="text-white font-bebas-neue text-[4rem]">
+        <p className="txtBelt">
+          MAKE YOU HEALHTY
+        </p>
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="50"
+          height="51"
+          viewBox="0 0 50 51"
+          fill="none"
+        >
+          <path
+            d="M25.0854 0.80481C25.0854 25.5 49.7806 25.5 49.7806 25.5C49.7806 25.5 25.0854 25.5 25.0854 50.1951C25.0854 25.5 0.390287 25.5 0.390287 25.5C0.390287 25.5 25.0855 25.5 25.0854 0.80481Z"
+            fill="white"
+          />
+        </svg>
+
+        <p className="txtBelt">
           MAKE YOU HEALHTY
         </p>
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="fill-none w-40 h-40"
+          width="50"
+          height="51"
           viewBox="0 0 50 51"
+          fill="none"
         >
           <path
             d="M25.0854 0.80481C25.0854 25.5 49.7806 25.5 49.7806 25.5C49.7806 25.5 25.0854 25.5 25.0854 50.1951C25.0854 25.5 0.390287 25.5 0.390287 25.5C0.390287 25.5 25.0855 25.5 25.0854 0.80481Z"
@@ -47,13 +51,15 @@ export const Belt = () => {
           />
         </svg>
 
-        <p className="text-white font-bebas-neue text-[4rem]">
+        <p className="txtBelt">
           MAKE YOU HEALHTY
         </p>
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="fill-none w-40 h-40"
+          width="50"
+          height="51"
           viewBox="0 0 50 51"
+          fill="none"
         >
           <path
             d="M25.0854 0.80481C25.0854 25.5 49.7806 25.5 49.7806 25.5C49.7806 25.5 25.0854 25.5 25.0854 50.1951C25.0854 25.5 0.390287 25.5 0.390287 25.5C0.390287 25.5 25.0855 25.5 25.0854 0.80481Z"
@@ -61,87 +67,15 @@ export const Belt = () => {
           />
         </svg>
 
-        <p className="text-white font-bebas-neue text-[4rem]">
+        <p className="txtBelt">
           MAKE YOU HEALHTY
         </p>
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="fill-none w-40 h-40"
+          width="50"
+          height="51"
           viewBox="0 0 50 51"
-        >
-          <path
-            d="M25.0854 0.80481C25.0854 25.5 49.7806 25.5 49.7806 25.5C49.7806 25.5 25.0854 25.5 25.0854 50.1951C25.0854 25.5 0.390287 25.5 0.390287 25.5C0.390287 25.5 25.0855 25.5 25.0854 0.80481Z"
-            fill="white"
-          />
-        </svg>
-      </div>
-      <div
-        className="flex items-center justify-center gap-4 w-[100vw] overflow-hidden h-[7.8rem] flex-nowrap"
-        id="belt-parenet-2"
-      >
-        {/* <div className="belt1 flex w-full" > */}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="fill-none w-40 h-40"
-          viewBox="0 0 50 51"
-        >
-          <path
-            d="M25.0854 0.80481C25.0854 25.5 49.7806 25.5 49.7806 25.5C49.7806 25.5 25.0854 25.5 25.0854 50.1951C25.0854 25.5 0.390287 25.5 0.390287 25.5C0.390287 25.5 25.0855 25.5 25.0854 0.80481Z"
-            fill="white"
-          />
-        </svg>
-
-        <p className="text-white font-bebas-neue text-[4rem]">
-          MAKE YOU HEALHTY
-        </p>
-        {/* </div> */}
-
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="fill-none w-40 h-40"
-          viewBox="0 0 50 51"
-        >
-          <path
-            d="M25.0854 0.80481C25.0854 25.5 49.7806 25.5 49.7806 25.5C49.7806 25.5 25.0854 25.5 25.0854 50.1951C25.0854 25.5 0.390287 25.5 0.390287 25.5C0.390287 25.5 25.0855 25.5 25.0854 0.80481Z"
-            fill="white"
-          />
-        </svg>
-
-        <p className="text-white font-bebas-neue text-[4rem]">
-          MAKE YOU HEALHTY
-        </p>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="fill-none w-40 h-40"
-          viewBox="0 0 50 51"
-        >
-          <path
-            d="M25.0854 0.80481C25.0854 25.5 49.7806 25.5 49.7806 25.5C49.7806 25.5 25.0854 25.5 25.0854 50.1951C25.0854 25.5 0.390287 25.5 0.390287 25.5C0.390287 25.5 25.0855 25.5 25.0854 0.80481Z"
-            fill="white"
-          />
-        </svg>
-
-        <p className="text-white font-bebas-neue text-[4rem]">
-          MAKE YOU HEALHTY
-        </p>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="fill-none w-40 h-40"
-          viewBox="0 0 50 51"
-        >
-          <path
-            d="M25.0854 0.80481C25.0854 25.5 49.7806 25.5 49.7806 25.5C49.7806 25.5 25.0854 25.5 25.0854 50.1951C25.0854 25.5 0.390287 25.5 0.390287 25.5C0.390287 25.5 25.0855 25.5 25.0854 0.80481Z"
-            fill="white"
-          />
-        </svg>
-
-        <p className="text-white font-bebas-neue text-[4rem]">
-          MAKE YOU HEALHTY
-        </p>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="fill-none w-40 h-40"
-          viewBox="0 0 50 51"
+          fill="none"
         >
           <path
             d="M25.0854 0.80481C25.0854 25.5 49.7806 25.5 49.7806 25.5C49.7806 25.5 25.0854 25.5 25.0854 50.1951C25.0854 25.5 0.390287 25.5 0.390287 25.5C0.390287 25.5 25.0855 25.5 25.0854 0.80481Z"
--- src/component/card/card.tsx
+++ src/component/card/card.tsx
@@ -1,4 +1,10 @@
-export const Card = (img) => {
+import React from "react"
+
+interface Props{
+  img: string
+}
+
+export const Card: React.FC<Props> = ({img}) => {
   return (
     <div className="rounded-sm">
         <img src={img} alt=""/>
