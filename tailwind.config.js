8d63a514e034f636ecbcf6283ff798f5a0c3b3fa
Syahrul <syahrul4w@gmail.com>
2023-10-08 23:45:00 +0700

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,27 +11,24 @@ export default {
     },
     screens: {
       'sm': '640px',
-      // => @media (min-width: 640px) { ... }
 
       'md': '768px',
-      // => @media (min-width: 768px) { ... }
 
       'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
 
       'xl': {
         'min': '1280px',
         'max': '1439px'
       },
-      // => @media (min-width: 1280px) { ... }
 
       '2xl': {
         'min': '1440px',
         'max': '1500px'
       },
-      // => @media (min-width: 1440px) { ... }
 
-      'laptop2': {'raw': '(width: 1280px) and (height: 800px)'},
+      'laptop2': {
+        'raw': '(width: 1280px) and (height: 800px)'
+      },
     }
   },
   plugins: [],
